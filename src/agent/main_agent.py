d37234a797f29b6d731c46c6eb7be1f7805d7a0b
developerdevananda <devananda88293@gmail.com>
2025-10-04 10:11:12 +0000

    started building agent

---
+++ src/agent/main_agent.py
@@ -0,0 +1,14 @@
+from langchain_Openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import intiaize_agent,  AgentType,  Tool
+
+
+
+def main_agent():
+
+     
+    llm = ChatOpenAI(
+        model='',
+        base_url ='',
+        api_key =''
+    )
