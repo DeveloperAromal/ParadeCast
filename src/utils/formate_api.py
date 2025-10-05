2fd70b24768857fbbafe1dea3e0bf89b2233cd8e
DeveloperAromal <developeraromal@gmail.com>
2025-10-05 17:07:29 +0530

    added api formatter

---
+++ src/utils/formate_api.py
@@ -0,0 +1,7 @@
+
+def formateapi(status_code, data, message):
+    return {
+                "status_code": status_code,
+                "data": data,
+                "message": message
+            }
